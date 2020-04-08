fe4879ff1618800c921d1a1569be6f9971a45ba1
vhostbase <vhostbase@gmail.com>
2020-04-08 17:00:04 +0530

    upd

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var app = express();
 
 app.use(express.static('public'));
 
-/*app.get('/', function (req, res) {
+app.get('/', function (req, res) {
     res.sendFile( __dirname + "/" + "index.html" );
-})*/
-var port = process.env.PORT | 8080
-var server = app.listen(port, function () {
+});
+//var port = process.env.PORT | 8080
+var server = app.listen(process.env.PORT, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("App listening at http://%s:%s", host, port)
